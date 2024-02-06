fe0cb03b60da3f14521c3bf4d2bb015c9ec32dd0
Deepak S <145679530+deepaksur@users.noreply.github.com>
2024-02-06 05:37:18 +0530

    Add files via upload

---
+++ electricitymanagement/conn.java
@@ -0,0 +1,16 @@
+package electricitymanagement;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "root"); // replace the password according to your account
+            s = c.createStatement();
+        } catch (Exception e) {
+        }
+    }
+}
